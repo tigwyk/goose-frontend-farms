6ce822d517903a7e0278b0b65685e1504ab28f6e
Lee Ingram <lee@soylentcola.com>
2021-09-21 17:17:47 -0700

    fix: Adding 250px logo for mobile

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -11,7 +11,7 @@ import TwitterCard from './components/TwitterCard'
 
 const Hero = styled.div`
   align-items: center;
-  background-image: url('/images/egg/rugpull_with_text.png');
+  background-image: url('/images/egg/rugpull_with_text_250px.png');
   background-repeat: no-repeat;
   background-position: top center;
   display: flex;
