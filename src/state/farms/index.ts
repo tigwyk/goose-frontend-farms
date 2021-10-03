e894954298ae1a5fb90a4364fde2a15fe1e19fb0
Lee Ingram <lee@soylentcola.com>
2021-10-03 11:46:19 -0700

    chore: Removing more debug

--- src/state/farms/index.ts
+++ src/state/farms/index.ts
@@ -39,7 +39,6 @@ export const { setFarmsPublicData, setFarmUserData } = farmsSlice.actions
 // Thunks
 export const fetchFarmsPublicDataAsync = () => async (dispatch) => {
   const farms = await fetchFarms()
-  console.log('Farms: ', farms)
   dispatch(setFarmsPublicData(farms))
 }
 export const fetchFarmUserDataAsync = (account) => async (dispatch) => {
